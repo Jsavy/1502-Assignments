a6b6272945373bc42a4a646cd906c8276afc905f
Justin <Justin@Poseidon.localdomain>
2021-01-29 17:18:29 -0700

    setters and getters for Player.java

--- Assignment 1 - Skeleton/src/mru/game/model/Player.java
+++ Assignment 1 - Skeleton/src/mru/game/model/Player.java
@@ -6,4 +6,38 @@ public class Player {
 	 * This class represent each player record in the Database
 	 * It is basically a model class for each record in the txt file
 	 */
+	
+	private String name;
+	private int score;
+	private int wins;
+	
+	public Player(String name, int score, int wins) {
+		this.name = name;
+		this.score = score;
+		this.wins = wins;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public int getWins() {
+		return wins;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public void setWins(int wins) {
+		this.wins = wins;
+	}
 }
