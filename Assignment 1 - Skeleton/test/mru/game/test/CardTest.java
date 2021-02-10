3925f46a86ae6634a9dba9c4e170ac9ef2bf1908
Jsavy <jsave586@mtroyal.ca>
2021-02-09 21:13:59 -0700

    Added some CardDeck test still in progress

--- Assignment 1 - Skeleton/test/mru/game/test/CardTest.java
+++ Assignment 1 - Skeleton/test/mru/game/test/CardTest.java
@@ -8,7 +8,7 @@ class CardTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+
 	}
 
 }
--- Assignment 1 - Skeleton/test/mru/game/test/DeckTest.java
+++ Assignment 1 - Skeleton/test/mru/game/test/DeckTest.java
@@ -4,11 +4,53 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import junit.framework.Assert;
+import mru.game.controller.*;
+
 class DeckTest {
 
+	private static final String Card = null;
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		CardDeck deck;
+		CardDeck deck2;
+		deck = new CardDeck();
+		deck2 = new CardDeck();
+		Assert.assertNotSame(deck, deck2);	
 	}
-
+	@Test
+	void test2() {
+		CardDeck deck = new CardDeck();
+		CardDeck decktest = new CardDeck();
+		Card currentCard = deck.getDeck().remove(0);
+		System.out.println(currentCard);
+		System.out.println(deck.getDeck().size());
+		System.out.println(decktest.getDeck().size());
+		
+	}
+	@Test
+	void test3() {
+		CardDeck deck = new CardDeck();
+		Card currentCard = deck.getDeck().remove(0);
+		Card secondCard = deck.getDeck().remove(0);
+		Assert.assertNotSame(currentCard, secondCard);
+		
+	}
+	@Test
+	void test4() {
+		CardDeck deck = new CardDeck();
+		Card testCard = deck.getDeck().remove(0);
+		System.out.println(testCard.getRank());
+		System.out.println(testCard.getSuit());
+	}
+	@Test
+	void test5() {
+		CardDeck deck = new CardDeck();
+		Card hand[] = null;
+		for(int i = 0; i <= 5; i++) {
+			Card testCard = deck.getDeck().remove(0);           // trying to make a hand but errors 
+			hand[i] = testCard;                                 // happen so not sure what ive done wrong
+		}
+		System.out.println(hand.toString());
+	}
 }
