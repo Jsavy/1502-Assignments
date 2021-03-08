2932bd0199b72259e030df1f6d22f32710e99942
Jsavy <jsave586@mtroyal.ca>
2021-03-08 13:58:03 -0700

    update to pull

--- Assignment 2/src/mru/tsc/model/Toy.java
+++ Assignment 2/src/mru/tsc/model/Toy.java
@@ -16,5 +16,45 @@ public abstract class Toy {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public int getSN() {
+		return SN;
+	}
+
+	public void setSN(int sN) {
+		SN = sN;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getAvaliableCount() {
+		return avaliableCount;
+	}
+
+	public void setAvaliableCount(int avaliableCount) {
+		this.avaliableCount = avaliableCount;
+	}
+
+	public String getAgeAppropriate() {
+		return ageAppropriate;
+	}
+
+	public void setAgeAppropriate(String ageAppropriate) {
+		this.ageAppropriate = ageAppropriate;
+	}
 	
 }
