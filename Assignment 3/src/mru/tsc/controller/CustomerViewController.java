6cdf249ae0d9cd5288a31b3455bbe691f2f0dc21
'Cynchronized' <'austin.thieu123@gmail.com'>
2021-04-03 21:19:03 -0600

    Changed category selection to choiceBox

--- Assignment 3/src/mru/tsc/controller/CustomerViewController.java
+++ Assignment 3/src/mru/tsc/controller/CustomerViewController.java
@@ -1,5 +1,39 @@
 package mru.tsc.controller;
 
-public class CustomerViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+
+
+public class CustomerViewController implements Initializable{
+	
+	@FXML
+	private ChoiceBox choiceBox;
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		
+		choiceBox.getItems().add("Figure");
+		choiceBox.getItems().add("Animal");
+		choiceBox.getItems().add("Board Game");
+		choiceBox.getItems().add("Puzzle");
+		
+		
+	}
+	
+	public void saveButtonPushed() {
+		
+	}
+	
+
+	
+	
+	
+
 	
 }
