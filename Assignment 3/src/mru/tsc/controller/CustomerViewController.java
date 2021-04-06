cf6ed5a334682870a72c83ef807ac8e2cc39fa62
'Cynchronized' <'austin.thieu123@gmail.com'>
2021-04-05 22:27:54 -0600

    added load file and exit file methods

--- Assignment 3/src/mru/tsc/controller/CustomerViewController.java
+++ Assignment 3/src/mru/tsc/controller/CustomerViewController.java
@@ -1,33 +1,148 @@
 package mru.tsc.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import mru.tsc.model.Animal;
+import mru.tsc.model.BoardGame;
+import mru.tsc.model.Figure;
+import mru.tsc.model.Puzzle;
+import mru.tsc.model.Toy;
 
 
 public class CustomerViewController implements Initializable{
+	
+	private static final String FILE_PATH = "res/toys.txt";
+	ArrayList<Toy> toys;
 	
 	@FXML
 	private ChoiceBox choiceBox;
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
+		toys = new ArrayList<Toy>();
 		
 		choiceBox.getItems().add("Figure");
 		choiceBox.getItems().add("Animal");
 		choiceBox.getItems().add("Board Game");
 		choiceBox.getItems().add("Puzzle");
 		
+		readFile();
+		
 		
 	}
 	
+
+	
 	public void saveButtonPushed() {
+	
+	}
+	
+	/**
+	 * A method which loads the file "toys.txt" and adds different types of toys into an arraylist
+	 */
+	private void readFile() {
+		File myFile = new File(FILE_PATH); // file location to read
+		String currentLine; // the current line that has been read of the file
+		String[] splittedLine; // the information for the whole line split into its components based on criteria
+		Scanner fileReader = null; //scanner for file reader
+		String text; // used for printing the error message 
+		int firstDigit; // the first digit of the serial number 
+		final int ZERO = 0; // used to determine the PolyMorphism 
+		final int ONE = 1; // used to determine the PolyMorphism
+		final int TWO = 2; // used to determine the PolyMorphism
+		final int THREE = 3; // used to determine the PolyMorphism
+		final int FOUR = 4; // used to determine the PolyMorphism
+		final int FIVE = 5; // used to determine the PolyMorphism
+		final int SIX = 6; // used to determine the PolyMorphism
+		final int SEVEN = 7; // used to determine the PolyMorphism
+		final int EIGHT = 8; // used to determine the PolyMorphism
+		final int NINE = 9; // used to determine the PolyMorphism
 		
+		try {
+			fileReader = new Scanner(myFile);
+		} catch (FileNotFoundException e) {
+			text = e.getMessage();
+		}
+
+			while(fileReader.hasNextLine()) {
+				currentLine = fileReader.nextLine();
+				splittedLine = currentLine.split(";");
+				firstDigit = Character.getNumericValue(splittedLine[ZERO].charAt(0));
+
+				// Loads in Figure toys
+				if (firstDigit == ZERO || firstDigit == ONE) {
+					
+					Toy f = new Figure ((splittedLine[ZERO]), splittedLine[ONE], splittedLine[TWO], 
+							Double.parseDouble(splittedLine[THREE]), Integer.parseInt(splittedLine[FOUR]), 
+							Integer.parseInt(splittedLine[FIVE]), splittedLine[SIX].charAt(ZERO));
+					toys.add(f);
+				// Loads in Animal toys
+				}else if (firstDigit == TWO || firstDigit == THREE){
+					
+					Toy a = new Animal ((splittedLine[ZERO]), splittedLine[ONE], splittedLine[TWO], 
+							Double.parseDouble(splittedLine[THREE]), Integer.parseInt(splittedLine[FOUR]), 
+							Integer.parseInt(splittedLine[FIVE]), splittedLine[SIX], splittedLine[SEVEN].charAt(0));
+					toys.add(a);
+				// Loads in Puzzle Ttoys
+				}else if (firstDigit == FOUR || firstDigit == FIVE || firstDigit == SIX){
+					
+					Toy p = new Puzzle ((splittedLine[ZERO]), splittedLine[ONE], splittedLine[TWO], 
+							Double.parseDouble(splittedLine[THREE]), Integer.parseInt(splittedLine[FOUR]), 
+							Integer.parseInt(splittedLine[FIVE]), splittedLine[SIX].charAt(ZERO));
+					toys.add(p);
+				// Loads in Board Game toys
+				}else if (firstDigit == SEVEN || firstDigit == EIGHT || firstDigit == NINE) {
+					
+					Toy b = new BoardGame ((splittedLine[ZERO]), splittedLine[ONE], splittedLine[TWO], 
+							Double.parseDouble(splittedLine[THREE]), Integer.parseInt(splittedLine[FOUR]), 
+							Integer.parseInt(splittedLine[FIVE]), splittedLine[SIX], splittedLine[SEVEN]);
+					toys.add(b);
+				}	
+			}
+			fileReader.close(); // close the file reader
+	}
+	
+	/**
+	 * Method which loads the toy data in the file "toys.txt"
+	 * 
+	 */
+	private void exitFile() {
+		File file = new File(FILE_PATH); // file location for export
+		PrintWriter pw; // used for file export
+		String text; // used to print exceptions messages
+			try {
+				pw = new PrintWriter(file);
+				for (Toy t: toys) {
+					if(t instanceof Animal) {
+						Animal cast = (Animal)t;
+						pw.println(cast.format());
+					} else if(t instanceof Figure) {
+						Figure cast = (Figure)t;
+						pw.println(cast.format());
+					} else if(t instanceof Puzzle) {
+						Puzzle cast = (Puzzle)t;
+						pw.println(cast.format());
+					} else if(t instanceof BoardGame) {
+						BoardGame cast = (BoardGame)t;
+						pw.println(cast.format());
+					}
+				}
+				
+				pw.close();
+			} catch (FileNotFoundException e) {
+				text = e.getMessage();
+			}
 	}
 	
 
