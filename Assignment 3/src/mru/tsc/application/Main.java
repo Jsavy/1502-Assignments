8a4c0ed58b8a921bc74d77eaad7b487d4da1d105
'Cynchronized' <'austin.thieu123@gmail.com'>
2021-04-07 16:20:28 -0600

    Added remove feature

--- Assignment 3/src/mru/tsc/application/Main.java
+++ Assignment 3/src/mru/tsc/application/Main.java
@@ -1,7 +1,16 @@
 package mru.tsc.application;
 	
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import mru.tsc.model.Animal;
+import mru.tsc.model.BoardGame;
+import mru.tsc.model.Figure;
+import mru.tsc.model.Puzzle;
+import mru.tsc.model.Toy;
 import javafx.scene.Scene;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
@@ -17,11 +26,17 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("../style/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			primaryStage.setOnCloseRequest(event -> {
+			
+		});
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+
+
 	public static void main(String[] args) {
 		launch(args);
 	}
